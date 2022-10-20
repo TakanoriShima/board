e59161067087d00cddd41f7e02e9022722d7ff44
TakanoriShima <quark2galaxy@gmail.com>
2022-10-20 10:28:43 +0900

    3. 新規投稿

--- src/main/java/com/example/board/BoardController.java
+++ src/main/java/com/example/board/BoardController.java
@@ -1,7 +1,10 @@
 package com.example.board;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -13,15 +16,45 @@ import com.example.board.PostFactory;
 @Controller
 public class BoardController {
 
-    /**
-    * 一覧を表示する。
-    *
-    * @param model モデル
-    * @return テンプレート
-    */
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String index(Model model) {
-        model.addAttribute("form", PostFactory.newPost());
-        return "layout";
-    }
+	@Autowired
+	private PostRepository repository;
+	
+	/** 投稿の一覧 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String index(Model model) {
+		model.addAttribute("form", PostFactory.newPost());
+		model = this.setList(model);
+		model.addAttribute("path", "create");
+		return "layout";
+	}
+
+	/**
+	 * 登録する。
+	 *
+	 * @param form  フォーム
+	 * @param model モデル
+	 * @return テンプレート
+	 */
+	@RequestMapping(value = "/create", method = RequestMethod.POST)
+	public String create(@ModelAttribute("form") Post form, BindingResult result, Model model) {
+		if (!result.hasErrors()) {
+			repository.saveAndFlush(PostFactory.createPost(form));
+			model.addAttribute("form", PostFactory.newPost());
+		}
+		model = this.setList(model);
+		model.addAttribute("path", "create");
+		return "layout";
+	}
+
+	/**
+	 * 一覧を設定する。
+	 *
+	 * @param model モデル
+	 * @return 一覧を設定したモデル
+	 */
+	private Model setList(Model model) {
+		Iterable<Post> list = repository.findAll();
+		model.addAttribute("list", list);
+		return model;
+	}
 }
